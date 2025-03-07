b67d8084483dc0c90054e1227b5c687c8169248d
Christopher Bayzand <cbayzand@F2K010.42adel.org.au>
2025-03-07 15:49:04 +1030

    adding minilibx

---
+++ src/main.c
@@ -0,0 +1,17 @@
+#include "minishell.h"
+
+int check_arg(char **argv, int argc)
+{
+	if (argc != 2)
+	{
+		printf("Error\nInput a single .rt file")
+		return (1);
+	}
+}
+
+int main(int argc, char **argv)
+{
+	if(check_arg(argv, argc))
+		return (1);
+	
+}
